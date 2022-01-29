b988538ca60e1c29ae7f48261a0aa31b7265c12f
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-01-29 17:15:32 +0530

    Added Entities

---
+++ source/services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿namespace Basket.API.Entities
+{
+
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+
+                Items.ForEach((item) =>
+                {
+                    totalprice += item.Price * item.Quantity;
+                });
+
+                return totalprice;
+                //foreach (var item in Items)
+                //{
+                //    totalprice += item.Price * item.Quantity;
+                //}
+
+            }
+        }
+    }
+
+}
